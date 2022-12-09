6e1e221cef2004f15deb6df42b4511c376c36a50
lgallagher <lewisjgallagher@gmail.com>
2022-12-09 10:42:49 +0000

    :sparkles: Day 8 part 2 solution
    Day 8 part 2 solution

--- 2022/Day 8 - Treetop Tree House/aoc.py
+++ 2022/Day 8 - Treetop Tree House/aoc.py
@@ -29,20 +29,22 @@ def _parse_input(data: str) -> List[int]:
 def is_visible(i: int, j: int, data: List[int]) -> bool:
     """Takes (x,y) coordinates as input and checks if the tree at that location is visible from the left, right, top or bottom respectively."""
 
-    if data[i][j] > max(data[i][:j]) or \
-        data[i][j] > max(data[i][j+1:]) or \
-        data[i][j] > max([row[j] for row in data[:i]]) or \
-        data[i][j] > max([row[j] for row in data[i+1:]]):
+    tree = data[i][j]
+
+    if tree > max(data[i][:j]) or \
+        tree > max(data[i][j+1:]) or \
+        tree > max([row[j] for row in data[:i]]) or \
+        tree > max([row[j] for row in data[i+1:]]):
         return True
 
 
 def part_1_solution(data: List[str]) -> int:
     """Compute solution to puzzle part 1."""
     
-    # All tree edges are visible. Get sum of shape.
+    # All tree edges are visible. Get sum of edges.
     visible = len(data) * 2 + len(data[0]) * 2 - 4
 
-    # Iterate through rows and columns.
+    # Iterate through inner rows and columns.
     for i in range(1, len(data)-1):
         for j in range(1, len(data[i])-1):
             if is_visible(i, j, data):
@@ -52,9 +54,42 @@ def part_1_solution(data: List[str]) -> int:
 
 
 def part_2_solution(data: List[str]) -> int:
-    """Compute solution to puzzle part 2."""
+    """Compute solution to puzzle part 2."""   
+    def get_distance(tree: int, neighbours: List[int]) -> int:
+        """Increment dist if neighbours are smaller than current tree"""
+        dist = 0
+        for neighbour in neighbours:
+            if neighbour >= tree:
+                dist += 1
+                return dist
+            else:
+                dist += 1
+        return dist
 
-    return 0
+    scenic_scores = []
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            tree = data[i][j]
+            left_neighbours = data[i][:j]
+            right_neighbours = data[i][j+1:]
+            up_neighbours = [row[j] for row in data[:i]]
+            down_neighbours = [row[j] for row in data[i+1:]]
+
+            # print(tree, data[i], left_neighbours, right_neighbours, up_neighbours, down_neighbours)
+            # Reverse left neighbours because it needs to iterate over neighbours as if beginning at the nearest neighbour
+            left, right, up, down = [
+                get_distance(tree, left_neighbours[::-1]),
+                get_distance(tree, right_neighbours),
+                get_distance(tree, up_neighbours[::-1]),
+                get_distance(tree, down_neighbours)
+            ]
+
+            scenic_score = 1
+            for s in [left, right, up, down]:
+                scenic_score = scenic_score * s
+            scenic_scores.append(scenic_score)
+
+    return max(scenic_scores)
 
 
 if __name__ == "__main__":
@@ -63,7 +98,7 @@ if __name__ == "__main__":
 
     # Assert the example input results are as expected.
     assert part_1_solution(example_data) == EXAMPLE_OUTPUT_PART1
-    # assert part_2_solution(example_data) == EXAMPLE_OUTPUT_PART2
+    assert part_2_solution(example_data) == EXAMPLE_OUTPUT_PART2
 
     # Read puzzle input.
     with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r", encoding="utf-8") as f:
@@ -71,5 +106,5 @@ if __name__ == "__main__":
 
     # Print answers
     print(f'Part 1: { part_1_solution(data=data) }')
-    # print(f'Part 2: { part_2_solution(data=data) }')
+    print(f'Part 2: { part_2_solution(data=data) }')
     print(timeit.timeit())
