6e57ed4a7e1aec34be4e0f9e265b4efcf2e0fd4c
lgallagher <lewisjgallagher@gmail.com>
2022-12-09 10:58:31 +0000

    :art: Simple formatting changes for readability
    Simple formatting changes for readability

--- 2022/Day 8 - Treetop Tree House/aoc.py
+++ 2022/Day 8 - Treetop Tree House/aoc.py
@@ -38,6 +38,18 @@ def is_visible(i: int, j: int, data: List[int]) -> bool:
         return True
 
 
+def get_visible_distance(tree: int, neighbours: List[int]) -> int:
+    """Increment dist if neighbours are smaller than current tree"""
+    dist = 0
+    for neighbour in neighbours:
+        if neighbour >= tree:
+            dist += 1
+            return dist
+        else:
+            dist += 1
+    return dist
+
+
 def part_1_solution(data: List[str]) -> int:
     """Compute solution to puzzle part 1."""
     
@@ -54,19 +66,10 @@ def part_1_solution(data: List[str]) -> int:
 
 
 def part_2_solution(data: List[str]) -> int:
-    """Compute solution to puzzle part 2."""   
-    def get_distance(tree: int, neighbours: List[int]) -> int:
-        """Increment dist if neighbours are smaller than current tree"""
-        dist = 0
-        for neighbour in neighbours:
-            if neighbour >= tree:
-                dist += 1
-                return dist
-            else:
-                dist += 1
-        return dist
-
+    """For each tree get it's neighbouring trees and find the distance it can see."""   
     scenic_scores = []
+
+    # Begin iterating over trees and get tree neighbours.
     for i in range(len(data)):
         for j in range(len(data[i])):
             tree = data[i][j]
@@ -75,18 +78,19 @@ def part_2_solution(data: List[str]) -> int:
             up_neighbours = [row[j] for row in data[:i]]
             down_neighbours = [row[j] for row in data[i+1:]]
 
-            # print(tree, data[i], left_neighbours, right_neighbours, up_neighbours, down_neighbours)
-            # Reverse left neighbours because it needs to iterate over neighbours as if beginning at the nearest neighbour
+            # Reverse left and up neighbours because it needs to iterate over neighbours as if beginning at the nearest neighbour
             left, right, up, down = [
-                get_distance(tree, left_neighbours[::-1]),
-                get_distance(tree, right_neighbours),
-                get_distance(tree, up_neighbours[::-1]),
-                get_distance(tree, down_neighbours)
+                get_visible_distance(tree, left_neighbours[::-1]),
+                get_visible_distance(tree, right_neighbours),
+                get_visible_distance(tree, up_neighbours[::-1]),
+                get_visible_distance(tree, down_neighbours)
             ]
 
+            # Multiply visible distances together to get the scenic score and return the maximum score.
+            # I think there's some numpy.prod magic I can do here instead...
             scenic_score = 1
-            for s in [left, right, up, down]:
-                scenic_score = scenic_score * s
+            for dist in [left, right, up, down]:
+                scenic_score = scenic_score * dist
             scenic_scores.append(scenic_score)
 
     return max(scenic_scores)
