d989bb2d94e9a61e50c2c3ee8beb3a8433a8aa25
lgallagher <lewisjgallagher@gmail.com>
2022-12-13 22:54:57 +0000

    :sparkles: Day 10 Part 1 solution

---
+++ 2022/Day 10 - Cathode-Ray Tube/aoc.py
@@ -0,0 +1,213 @@
+"""
+Solution for Advent of Code 2022 day 10 - https://adventofcode.com/2022/day/10
+"""
+
+import os
+from typing import List, Any
+import timeit
+
+
+EXAMPLE_INPUT = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop'''
+
+EXAMPLE_OUTPUT_PART1 = 13140
+EXAMPLE_OUTPUT_PART2 = 0
+
+
+def _parse_input(data: str) -> List[str]:
+    """Parse input text file into usable data structure."""
+
+    return [i.split() for i in data.splitlines()]
+
+
+
+def part_1_solution(data: List[str]) -> Any:
+    """Compute solution to puzzle part 1."""
+    
+    # Count cycles and initialise X signal value.
+    X = 1
+    signals = []
+
+    # Loop over each action and append the X value at the end of each action
+    for action in data:
+        
+        # addx takes 2 cycles (range(2)) and updates X at the end of these cycels.
+        if action[0] == 'addx':
+            for _ in range(2):
+                signals.append(X)
+            X += int(action[1])
+
+        elif action[0] == 'noop':
+            # noop takes 1 cycle and doesn't alter X so append X as is.
+            signals.append(X)
+
+    return sum([signals[i-1] * i for i in range(20,len(signals), 40)])
+
+
+def part_2_solution(data: List[str]) -> Any:
+    """Compute solution to puzzle part 2."""
+    return 0
+
+
+if __name__ == "__main__":
+    # Compute puzzle with example data
+    example_data = _parse_input(EXAMPLE_INPUT)
+    print(example_data)
+    # Assert the example input results are as expected.
+    print(part_1_solution(example_data))
+    assert part_1_solution(example_data) == EXAMPLE_OUTPUT_PART1
+    # assert part_2_solution(example_data) == EXAMPLE_OUTPUT_PART2
+
+    # Read puzzle input.
+    with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r", encoding="utf-8") as f:
+        data = _parse_input(f.read())
+
+    # # Print answers
+    print(f'Part 1: { part_1_solution(data=data) }')
+    # print(f'Part 2: { part_2_solution(data=data) }')
+    print(timeit.timeit())
