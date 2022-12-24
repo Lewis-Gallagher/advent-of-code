73408bbef18ebff45f8434caf5502be37cbf1aee
lgallagher <lewisjgallagher@gmail.com>
2022-12-24 01:46:58 +0000

    :recycle: Refactored inspect Monkey class and functions
    Changed solution input to take a dict of Monkeys

--- 2022/Day 11 - Monkey in the Middle/aoc.py
+++ 2022/Day 11 - Monkey in the Middle/aoc.py
@@ -3,13 +3,14 @@ Solution for Advent of Code 2022 day 11 - https://adventofcode.com/2022/day/11
 Lewis Gallagher - lewis.gallagher@icr.ac.uk
 """
 
+from __future__ import annotations
 import os
-from typing import List, Any
-import timeit
+from typing import List, Dict
+import time
 from collections import deque
-from dataclasses import dataclass
 import re
 from math import prod
+
 
 
 EXAMPLE_INPUT = '''\
@@ -44,25 +45,90 @@ Monkey 3:
 EXAMPLE_OUTPUT_PART1 = 10605
 EXAMPLE_OUTPUT_PART2 = 0
 
-@dataclass
 class Monkey:
-    num: int
-    current_items: deque
-    operation: str
-    test: bool
-    true_idx: int
-    false_idx: int
-    inspected_count: int
+    """
+    The Monkey class possesses many attributes.
+    current_items represents the all the items a monkey has and their worry levles.
+    operation represents how the worry level of each item should change upon inspection.
+    """
+    def __init__(self, monkey_num: int, current_items: List[int], operation: str, divisor: int, true_idx: int, false_idx: int) -> None:
+        self.monkey_num = monkey_num # E.g. 0
+        self.current_items = current_items # E.g. [11,65,75]
+        self.operation = operation # E.g. ['*', '8']
+        self.divisor = divisor # E.g. 23
+        self.true_idx = true_idx # E.g. 1
+        self.false_idx = false_idx # E.g. 2
+        self.inspected_count = 0 # Start at zero
+    
+    def __repr__(self) -> str:
+        return f"Monkey:(id={self.monkey_num}, items={self.current_items}, inspect_count={self.inspected_count})"
 
-    def from_data(monkey: List[str]):
 
-        # monkey_dict = dict()
-        
-        # Find monkey properties.
+    def inspect_item(self) -> int:
+        """Inspects the next item in a Monkey's item list. 
+        Inspecting causes our worry level to go up, before it is divided by three and rounded down. 
+        Then we test who to throw to, by dividing by a divisor."""
+
+        # Find if the operation is multiplication or addition and compute operation accordingly:
+        # new = old * int
+        # new = old + int
+        # new = old * old
+        # new = old + old
+
+        try:
+            if self.operation[0] == '*' and self.operation[1] == 'old':
+                self.current_items[0] = self.current_items[0]**2
+
+            elif self.operation[0] == '*' and self.operation[1].isnumeric():
+                self.current_items[0] = self.current_items[0] * int(self.operation[1])
+
+            elif self.operation[0] == '+' and self.operation[1] == 'old':
+                self.current_items[0] = self.current_items[0] + self.current_items[0]
+
+            elif self.operation[0] == '+' and self.operation[1].isnumeric():
+                self.current_items[0] = self.current_items[0] + int(self.operation[1])
+
+        except ValueError:
+            print('Operation could not be completed.')
+
+        # Reduce our worry level and increment inspected counter.
+        self.current_items[0] = self.current_items[0] // 3
+        self.inspected_count += 1
+ 
+        # Return an int representing which monkey to throw to.
+        if self.current_items[0] % self.divisor == 0:
+            return self.true_idx    
+        else:
+            return self.false_idx
+
+
+    def add_item(self, item: int):
+        """Receives an item and appends it to end of monkey's item list."""
+        self.current_items.append(item)
+
+
+    def give_item(self, other: Monkey) -> int:
+        """Adds current item to the item list of a monkey while removing from the list of the current monkey."""
+        other.add_item(self.current_items.popleft())        
+
+
+
+def _parse_input(data: str) -> List[str]:
+    """
+    Reads the input data into a list of lists, where each sublist represents a monkey.
+    Then regex out all the relevant numbers and create a Monkey class object.
+    Append Monkey object to a dictionairy of Monkeys where the keys are the monkey_num.
+    """
+
+    monkey_dict = dict()
+
+    monkey_list = [[monkey.strip() for monkey in monkies.split('\n')] for monkies in data.strip().split('\n\n')]
+
+    for monkey in monkey_list:
         monkey_number = re.findall(r"^\s*Monkey (\d)", monkey[0])[0]
         starting_items = re.findall(r"^\s*Starting items: (\d+.*)", monkey[1])
         operation = re.findall(r"^\s*Operation: new = old ([+*] .+)", monkey[2])
-        test_if = re.findall(r"^\s*Test: divisible by (\d*)", monkey[3])[0]
+        divisor = re.findall(r"^\s*Test: divisible by (\d*)", monkey[3])[0]
         test_true_throw = re.findall(r"^\s*If true: throw to monkey (\d*)", monkey[4])[0]
         test_false_throw = re.findall(r"^\s*If false: throw to monkey (\d*)", monkey[5])[0]
 
@@ -70,123 +136,61 @@ class Monkey:
         starting_items = [int(item) for item in ' '.join(starting_items).split(', ')]
         operation = [op for op in ' '.join(operation).split(' ')]
 
-        return Monkey(
-            num = int(monkey_number),
+        monkey =  Monkey(
+            monkey_num = int(monkey_number),
             current_items = deque(starting_items),
             operation = operation,
-            test = int(test_if),
+            divisor = int(divisor),
             true_idx = int(test_true_throw),
-            false_idx = int(test_false_throw),
-            inspected_count = 0
+            false_idx = int(test_false_throw)
         )
 
-def inspect_item(list_of_monkeys: List[Monkey], part_2: bool):
-    """Accepts a list of monkeys and loops over each of their items. Alter's the item's worry value by an operation and passes the item to another monkey based on the output of a modulo expression. The item is removed from the original monkey's item list and added to the new monkey's item list."""
+        monkey_dict[monkey_number] = monkey
 
-    for monkey in list_of_monkeys:
-
-        for item in list(monkey.current_items):
-            
-            # Compute new item value.
-            item_new_value = compute_operation(monkey.operation, item, part_2)
-
-            # Throw item to monkey based on result of test.
-            if item_new_value % monkey.test == 0:
-                list_of_monkeys[monkey.true_idx].current_items.append(item_new_value)
-            else:
-                list_of_monkeys[monkey.false_idx].current_items.append(item_new_value)
-
-            # Remove item for monkey's item list and increment inspected count.
-            monkey.current_items.popleft()
-            monkey.inspected_count += 1
-
-    return list_of_monkeys
+    return monkey_dict
 
 
-def compute_operation(operation: List[str], item: int, part_2: bool) -> int:
-    """Shows how an item's worry level changes as that monkey inspects an item. Worry levels are multiplied by or added to a number. Operations can be of the following formats:
-    new = old * old
-    new = old * int
-    new = old + old
-    new  = old + int"""
-
-    try:
-        if operation[0] == '*' and operation[1] == 'old':
-             new_item_val =  int(item**2)
-
-        elif operation[0] == '*' and operation[1].isnumeric():
-            new_item_val = int(item * int(operation[1]))
-
-        elif operation[0] == '+' and operation[1] == 'old':
-            new_item_val = int(item + item)
-
-        elif operation[0] == '+' and operation[1].isnumeric():
-            new_item_val = int(item + int(operation[1]))
-
-    except ValueError:
-        print('Operation could not be completed.')
-
-    if part_2 == False:
-        return new_item_val // 3
-    else:
-        return new_item_val
-
-def _parse_input(data: str) -> List[str]:
-    """Returns nested list, where each list is a monkey."""
-
-    return [[monkey.strip() for monkey in monkies.split('\n')] for monkies in data.strip().split('\n\n')]
-
-
-def barrel_of_monkeys(data: List[str]) -> List[Monkey]:
-    """Generates a list of Monkeys."""
-
-    monkeys_list = list()
-    
-    for monkey in data:
-        monkeys_list.append(Monkey.from_data(monkey))
-        
-    return monkeys_list
-
-
-def part_1_solution(data: List[str]) -> int:
+def part_1_solution(monkey_dict: Dict[int, Monkey]) -> int:
     """Compute solution to puzzle part 1."""
 
-    monkey_list = barrel_of_monkeys(data)
+    # 20 rounds of play.
+    rounds_to_play = 20
 
-    for _ in range(20):
-        monkey_list = inspect_item(monkey_list, part_2=False)
-    
-    return prod(sorted(monkey.inspected_count for monkey in monkey_list)[-2:])
+    for _ in range(1, rounds_to_play+1):
+        for monkey in monkey_dict.values(): # Iterator through monkeys in order.
+            while monkey.current_items: # Monkey inspects and throws until it has no more items.
+                to_monkey = list(monkey_dict.values())[monkey.inspect_item()] # Find which monkey to throw to.
+                monkey.give_item(to_monkey) # Throw to the chosen monkey.
+
+    # Return monkey business. i.e. the total of two highest inspection_counts multiplied together.
+    return prod(sorted(monkey.inspected_count for monkey in monkey_dict.values())[-2:])
+
 
 def part_2_solution(data: List[str]) -> Any:
     """Compute solution to puzzle part 2."""
-
-    monkey_list = barrel_of_monkeys(data)
-
-    for _ in range(10000):
-        monkey_list = inspect_item(monkey_list, part_2=True)
-    
-    return prod(sorted(monkey.inspected_count for monkey in monkey_list)[-2:])
 
     return 0
 
 
 if __name__ == "__main__":
-    # Compute puzzle with example data
+    # Compute puzzle with example data.
     example_data = _parse_input(EXAMPLE_INPUT)
 
     # Assert the example input results are as expected.
-    # assert part_1_solution(example_data) == EXAMPLE_OUTPUT_PART1
+    assert part_1_solution(example_data) == EXAMPLE_OUTPUT_PART1
     # assert part_2_solution(example_data) == EXAMPLE_OUTPUT_PART2
-    print(part_1_solution(example_data))
-    # print(part_2_solution(example_data))
-
 
     # Read puzzle input.
     with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r", encoding="utf-8") as f:
         data = _parse_input(f.read())
 
-    # # Print answers
-    print(f'Part 1: { part_1_solution(data=data) }')
-    # print(f'Part 2: { part_2_solution(data=data) }')
-    print(timeit.timeit())
+    # Print answers.
+    start_time_1 = time.time()
+    print(f'Part 1: { part_1_solution(data) }')
+    execution_time_1 = (time.time() - start_time_1)
+    print('Part 1 execution time: ' + str(execution_time_1))
+
+    # start_time_2 = time.time()
+    # print(f'Part 2: { part_2_solution(data) }')
+    # execution_time_2 = (time.time() - start_time_2)
+    # print('Part 2 execution time: ' + str(execution_time_2))
